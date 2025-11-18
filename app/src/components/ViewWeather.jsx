e173378fcb2a53031952dae953dddf5f68daa501
Rúbia Cacemiro <100322585+RobertaCacemiro@users.noreply.github.com>
2025-11-18 12:59:08 +0000

    Iniciando Visualização das Informações da API

--- app/src/components/ViewWeather.jsx
+++ app/src/components/ViewWeather.jsx
@@ -10,14 +10,35 @@ function ViewWeather({ data }) {
         <h1 className="text-black">{data.name}</h1>
       </div>
 
-      <div className="card w-96 bg-base-100 card-xl shadow-sm bg-white">
-        <div className="card-body text-black">
-          <div class="grid grid-flow-col grid-rows-2 gap-4">
-            <div class="row-span-3">TESTE 1</div>
-            <div class="col-span-2">TESTE 2</div>
-            <div class="col-span-2 row-span-2">TESTE 3</div>
+      <div className="card w-96 card-xl shadow-sm bg-white p-0">
+        {/* <div className="card-body text-black"> */}
+        <div class="grid grid-flow-col grid-rows-2 gap-4">
+          <div class="col-span-2 bg-red-300">{data.main?.temp}</div>
+          <div class="col-span-2 bg-green-300">
+            <div className="hover-3d">
+              {/* content */}
+              <figure className="w-60 rounded-2xl">
+                <img
+                  src="https://openweathermap.org/img/wn/04d@2x.png"
+                  alt="Tailwind CSS 3D card"
+                />
+              </figure>
+              {/* 8 empty divs needed for the 3D effect */}
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+
+            <div className="hover-3d"></div>
           </div>
+          <div class="row-span-3 bg-yellow-300">{data.main?.temp}</div>
         </div>
+        {/* </div> */}
       </div>
     </div>
   );
